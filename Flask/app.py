4a0d1a5bcb61b69ebc47479bff2e03e2c117d631
Davicious27 <86326278+Davicious27@users.noreply.github.com>
2022-05-23 17:37:52 -0500

    Updating last changes

---
+++ Flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Iniciar la aplicación
+
+app = Flask(__name__)
+
+#Definiendo Root path
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+
+# Ets línea se agrega si se ejecuta el archivo
+# directamente con el comando python
+#app.run()
